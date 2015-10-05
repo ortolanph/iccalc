23bc71c91d30197293ccb565e045261f4311bd60
Paulo Henrique Ortolan <paulo.ortolan@serpro.gov.br>
2015-10-05 17:00:51 -0300

    Algumas correções

--- src/main/java/org/iccalc/service/CalculatorService.java
+++ src/main/java/org/iccalc/service/CalculatorService.java
@@ -4,19 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-    public int adicao(int parcela1, int parcela2) {
-        return (parcela1 + parcela2);
-    }
+    public int adicao(int parcela1, int parcela2) { return (parcela1 + parcela2); }
 
-    public int subtracao(int parcela1, int parcela2) {
-        return (parcela1 - parcela2);
-    }
+    public int subtracao(int parcela1, int parcela2) { return (parcela1 - parcela2); }
 
-    public long multiplicacao(long fator1, long fator2) {
-        return (fator1 * fator2);
-    }
+    public long multiplicacao(long fator1, long fator2) { return (fator1 * fator2);  }
 
-    public double divisao(int dividendo, int divisor) {
-        return (dividendo / divisor);
-    }
+    public double divisao(int dividendo, int divisor) { return (dividendo / divisor); }
 }
