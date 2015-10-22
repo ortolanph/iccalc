5c90308a1789b112f1bac0715d8796db4f14064c
Paulo Henrique Ortolan <paulo.ortolan@serpro.gov.br>
2015-10-22 15:26:51 -0200

    Código corrigido.

--- src/main/java/org/iccalc/service/AdvancedCalculator.java
+++ src/main/java/org/iccalc/service/AdvancedCalculator.java
@@ -3,7 +3,6 @@ package org.iccalc.service;
 public class AdvancedCalculator {
 
     public long factorial(int i) {
-        return 0;
-        //return (i == 0)? 1 : i * factorial(i - 1);
+        return (i == 0)? 1 : i * factorial(i - 1);
     }
 }
